b82d2e55cf387da57bddea9dc3edfb797cb48ed2
plzmlc777 <plzmlc@outlook.com>
2026-02-26 20:16:17 +0900

    v1.1.0.42: Version bump

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,7 +13,7 @@ load_dotenv("backend/.env")    # child (if running from root)
 class Settings(BaseSettings):
     APP_ENV: str = "dev"
     PROJECT_NAME: str = "My Auto Trading"
-    PROJECT_VERSION: str = "1.1.0.41"
+    PROJECT_VERSION: str = "1.1.0.42"
     BACKEND_PORT: int = 8001
     FRONTEND_PORT: int = 5173
     
